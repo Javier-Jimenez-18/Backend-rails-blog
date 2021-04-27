bbbcb11e66b4e40226c32f909d63fee08b44b7b9
Javier Jimenez <javierjimenez@Deborahs-iPhone.GreenAcresLAN>
2021-04-27 01:45:11 -0700

    Treehouse Lesson6 completed Routes to Update Actions

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,16 +14,39 @@ class PagesController < ApplicationController
   end
 
   def create
-    #@page = Page.new(params) (doesn't specify which params are allowed for use)
-    #render text: params.to_json (just to see the object notation)
+    # @page = Page.new(params) (doesn't specify which params are allowed for use)
+    # render text: params.to_json (just to see the object notation)
 
     # require only the page parameter, permit only the three page parameters
-    page_params = params.require(:page).permit(:title, :body, :slug)
-    # set instance var @page to the new Page Object with page_params attributes
+    # page_params = params.require(:page).permit(:title, :body, :slug)
+
+    # set instance var @page to the new Page Object with the filtered page_params
     @page = Page.new(page_params)
     # save the Page record
     @page.save
     # tell browser to load the view for the new page once it's saved
     redirect_to @page
   end
+
+  def edit
+    @page = Page.find(params[:id])
+  end
+
+  def update
+    @page = Page.find(params[:id])
+
+    # require only the page parameter, permit only the three page parameters
+    # page_params = params.require(:page).permit(:title, :body, :slug)
+
+    # pass the filtered page_params to the Page Object loaded in
+    @page.update(page_params)
+    # tell browser to load the view for the updated page once it's saved
+    redirect_to @page
+  end
+
+  private
+    # require only the page parameter, permit only the three page parameters
+    def page_params
+      params.require(:page).permit(:title, :body, :slug)
+    end
 end
